43b116f6a1c75a296b9cb78a06d50b9cdccfc6bd
LyxZadric <fleur.algernon@gmail.com>
2017-05-25 10:28:33 +0200

    require header + index.php

---
+++ intégration/layout/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Arctrek</title>
+    <link rel="stylesheet" href="styles/css/style.css">
+    <link rel="stylesheet" href="styles/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="styles/css/bootstrap.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="favicons/manifest.json">
+    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+</head>
+
+<body>
+
+    <div class="container-fluid">
+
+        <header>
+            <!-- logo + navigation principale et secondaire-->
+            <div class="row">
+                <div class="col-4-md col-lg-4 col-sm-12 col-xs-12 logo-container">
+                    <img src="images-finales/logo.png" alt="paysage" class="logo">
+                </div>
+                <div class="col-8-md col-lg-8 col-sm-12 col-xs-12">
+                    <div class="row">
+                        <ul class="nav-secondaire">
+                            <li><a href="#">Newsletter</a></li>
+                            <li><a href="#">Brochure</a></li>
+                            <li><a href="#">Espace Clients</a></li>
+                            <li><a href="#">Panier(0)</a></li>
+                        </ul>
+
+
+                    </div>
+                    <div class="row">
+                        <nav class="main-nav">
+                            <ul class="nav-principale">
+                                <li><a href="#">Accueil</a></li>
+                                <li><a href="#">Nos destinations</a></li>
+                                <li><a href="#">Nos séjours</a></li>
+                                <li><a href="#">Le blog</a></li>
+                                <li><a href="#">A propos</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </nav>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Image header -->
+            <div class="row">
+                <div class="col">
+                    <img src="images-finales/Image-header.jpg" alt="paysage" class="img-fluid">
+                </div>
+            </div>
+
+            <!-- Barre réseaux sociaux -->
+            <div class="row">
+                <div class="col reseaux-sociaux">
+                    <p>Retrouvez-nous sur :</p>
+                    <ul class="list-social">
+                        <li><a href="#" class="hidden-text">
+                            <img src="images-finales/icones/face2.png" alt="" class="img-fluid img-reseaux">
+                        </a></li>
+                        <li><a href="#" class="hidden-text">
+                            <img src="images-finales/icones/twit2.png" alt="" class="img-fluid img-reseaux">
+                        </a></li>
+                        <li><a href="#" class="hidden-text">
+                            <img src="images-finales/icones/insta2.png" alt="" class="img-fluid img-reseaux">
+                        </a></li>
+                        <li><a href="#" class="hidden-text">
+                            <img src="images-finales/icones/face2.png" alt="" class="img-fluid img-reseaux">
+                        </a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </header>
